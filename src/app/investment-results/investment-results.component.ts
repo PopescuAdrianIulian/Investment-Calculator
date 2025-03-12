c8e5ef2cc20c472a9be140fb78a1117e3709e259
AdiPopescu <adipopescu2234@gmail.com>
2025-03-12 19:34:00 +0200

    Initial commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,19 @@
+import {Component, computed, inject} from '@angular/core';
+import {CurrencyPipe} from "@angular/common";
+import {InvestmentService} from "../investment.service";
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [
+    CurrencyPipe
+  ],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+
+  private service = inject(InvestmentService);
+
+  results = computed(() => this.service.resultData());
+}
